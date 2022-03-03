07a7af711ca4edd37e1ceabef91b7d7245749b7e
马腾飞 <qiuyun.ma@tuya.com>
2022-03-03 15:36:28 +0800

    feat: 优化代码

--- src/web_spider/jd_discount.js
+++ src/web_spider/jd_discount.js
@@ -5,9 +5,40 @@ const PAGE = 'https://a.jd.com';
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false
+    headless: true,
   });
   const page = await browser.newPage();
+
+  page.once('loaded', async () => {
+    const results = await page.$eval('body', (body) => {
+      document.documentElement.scrollTop = document.body.clientHeight;
+      return {
+        body: [...body.querySelectorAll('#quanlist > .quan-item')].map(
+          (item) => {
+            return {
+              name: item.querySelector('.q-price').innerText,
+              range: item.querySelector('.q-range').innerText,
+              process: item.querySelector('.q-progress')
+                ? item.querySelector('.q-progress').innerText.replace(/\n/g, '')
+                : `开始时间：${
+                    item.querySelector('.count-time .quan-time').dataset.end
+                  }`,
+            };
+          },
+        ),
+      };
+    });
+
+    const writer = fs.createWriteStream('./jd_discount.txt');
+    writer.write(`优惠券地址：${PAGE}\n`, 'utf8');
+    results.body.map((res) => {
+      writer.write(`优惠券：${res.name}\n`, 'utf8');
+      writer.write(`范围：${res.range}\n`, 'utf8');
+      writer.write(`进度：${res.process}\n\n`, 'utf8');
+    });
+    writer.close();
+    browser.close();
+  });
 
   await page.goto(PAGE);
 
@@ -15,31 +46,5 @@ const PAGE = 'https://a.jd.com';
     document.documentElement.scrollTop = document.body.clientHeight;
   });
 
-  await page.content();
-
-  const results = await page.$eval('body', (body) => {
-    document.documentElement.scrollTop = document.body.clientHeight;
-    return {
-      body: [...body.querySelectorAll('#quanlist > .quan-item')].map((item) => {
-        return {
-          name: item.querySelector('.q-price').innerText,
-          range: item.querySelector('.q-range').innerText,
-          process:
-            item.querySelector('.q-progress') ?
-            item.querySelector('.q-progress').innerText.replace(/\n/g, '') : `开始时间：${item.querySelector('.count-time .quan-time').dataset.end}`
-        };
-      })
-    };
-  });
-
-  const writer = fs.createWriteStream('./jd_discount.txt');
-  writer.write(`优惠券地址：${PAGE}\n`, 'utf8');
-  results.body.map((res) => {
-    writer.write(`优惠券：${res.name}\n`, 'utf8');
-    writer.write(`范围：${res.range}\n`, 'utf8');
-    writer.write(`进度：${res.process}\n\n`, 'utf8');
-  });
-  writer.close();
-
-  await browser.close();
+  await page.emit('loaded');
 })();
